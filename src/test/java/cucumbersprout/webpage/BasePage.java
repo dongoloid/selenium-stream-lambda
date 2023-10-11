f23979f4d67aa9cbb9c9fcc13b0816cde93e9e63
Rhodenel Omaña <rhodenel.omana@ph.softvision.global>
2023-10-11 17:06:11 +0800

    setup teardown

--- src/test/java/cucumbersprout/webpage/BasePage.java
+++ src/test/java/cucumbersprout/webpage/BasePage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
